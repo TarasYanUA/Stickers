6b9fd5a916c7f3ae7658a42ad6d0d8752fcb3825
TarasYanUA <wildmonach@gmail.com>
2023-02-13 18:09:56 +0200

    Starts working with test-case 2.

--- src/main/java/adminPanel/ProductSettings.java
+++ src/main/java/adminPanel/ProductSettings.java
@@ -9,11 +9,13 @@ public class ProductSettings {
 
     public SelenideElement statusActive_Product = $("#elm_product_status_0_a");
     public SelenideElement field_ListPrice = $("#elm_list_price");
+    public SelenideElement field_productSearch = $("#simple_search input");
     public SelenideElement productName = $(".product-name-column a");
     public SelenideElement productTemplate = $("#elm_details_layout");
     public SelenideElement tab_General = $("#detailed");
     public SelenideElement tab_Shippings = $("#shippings");
     public  SelenideElement field_ProductWeight = $("#product_weight");
+    public  SelenideElement button_SearchProduct = $(".advanced-search-field__search");
 
 
     public Select getProductTemplate(){return new Select(productTemplate);}
@@ -24,5 +26,10 @@ public class ProductSettings {
         field_ProductWeight.click();
         field_ProductWeight.clear();
         field_ProductWeight.sendKeys(value);
+    }
+    public void clickAndType_ProductSearch(String value){
+        field_productSearch.click();
+        field_productSearch.sendKeys(value);
+        button_SearchProduct.click();
     }
 }
--- src/test/java/TestCaseTwo.java
+++ src/test/java/TestCaseTwo.java
@@ -16,9 +16,8 @@ import static com.codeborne.selenide.Selenide.$x;
 - Модуль "Видео галерея": Вертикальное отображение галереи
 - Модуль "Стикеры":
     - Позиция отображения: слева + сверху/снизу
-    - Способ отображения: в колонку
+    - Способ отображения: в строку
     - Позиция пиктограмм: Позиция 1
-    - Описание к стикеру
 - Страница товара: все шаблоны
 - Страница категории: все шаблоны + окно Быстрого просмотра
 - Блок с товарами: шаблоны АВ: Сетка (с кнопкой "Показать ещё") + АВ: Расширенный скроллер товаров
@@ -27,149 +26,30 @@ import static com.codeborne.selenide.Selenide.$x;
 public class TestCaseTwo extends TestRunner {
     @Test(priority=1)
     public void TestCaseOne_ConfigureSettings() {
-        //Включаем мини-иконки в виде галереи и окно Быстрого просмотра
-        CsCartSettings csCartSettings = new CsCartSettings();
-        csCartSettings.menuSettings.hover();
-        csCartSettings.sectionAppearance.click();
-        if (!csCartSettings.settingMiniThumbnailAsGallery.isSelected()) {
-            csCartSettings.settingMiniThumbnailAsGallery.click();
-        }
-        if (!csCartSettings.settingQuickView.isSelected()) {
-            csCartSettings.settingQuickView.click();
-        }
-        csCartSettings.button_Save.click();
-
-        //Включаем Вертикальное отображение мини-иконок (Модуль "Видео галерея")
-        csCartSettings.navigateToAddonsPage();
-        VideoGallerySettings videoGallerySettings = csCartSettings.navigateToVideoGalleryPage();
-        videoGallerySettings.tabSettings.click();
-        if (!videoGallerySettings.settingVerticalView.isSelected()) {
-            videoGallerySettings.settingVerticalView.click();
-            videoGallerySettings.buttonSaveVideoGallery.click();
-        }
-
-        //Настраиваем позицию пиктограмм
-        csCartSettings.navigateToAddonsPage();
-        UniThemeSettings uniThemeSettings = csCartSettings.navigateToUniThemeSettings();
-        uniThemeSettings.tab_ProductList.click();
-        uniThemeSettings.selectPictogramPosition_Grid("position_1");
-        uniThemeSettings.selectPictogramPosition_ListWithoutOptions("position_1");
-        uniThemeSettings.selectPictogramPosition_CompactList("position_1");
-        uniThemeSettings.tab_Product.hover().click();
-        uniThemeSettings.selectPictogramPosition_Product("position_1");
-        csCartSettings.button_Save.click();
-
-        //Настраиваем блок с товарами
-        csCartSettings.menuDesign.hover();
-        csCartSettings.sectionLayouts.click();
-        csCartSettings.layout_TabProducts.click();
-        csCartSettings.layout_GearwheelOfBlockPopular.click();
-        csCartSettings.popupWindow.shouldBe(Condition.enabled);
-        csCartSettings.selectBlockTemplate("blocks/products/ab__grid_list.tpl");
-        csCartSettings.layoutBlock_TabContent.click();
-        csCartSettings.selectLayout_FieldFilling();
-        csCartSettings.clickAndType_Layout_FieldMaxLimit();
-        csCartSettings.layout_ButtonSaveBlock.click();
-        csCartSettings.layout_GearwheelOfBlockHits.click();
-        csCartSettings.popupWindow.shouldBe(Condition.enabled);
-        csCartSettings.selectBlockTemplate("blocks/products/products_scroller_advanced.tpl");
-        csCartSettings.layout_ButtonSaveBlock.click();
-
         //Настраиваем настройки модуля "Стикеры"
+        CsCartSettings csCartSettings = new CsCartSettings();
         csCartSettings.navigateToAddonsPage();
         StickerSettings stickerSettings = csCartSettings.navigateToStickerSettingsPage();
         stickerSettings.tabSettings.click();
         stickerSettings.selectSettingOutputPosition("L");
-        stickerSettings.selectSettingOutputTypeTop("column");
+        stickerSettings.selectSettingOutputTypeTop("row");
         stickerSettings.selectSettingMaxNumberTop("3");
-        stickerSettings.selectSettingOutputTypeBottom("column");
+        stickerSettings.selectSettingOutputTypeBottom("row");
         stickerSettings.selectSettingMaxNumberBottom("3");
         stickerSettings.buttonSaveSettings.click();
-        //Три верхних стикера
-        csCartSettings.navigateToAddonsPage();
-        csCartSettings.navigateToStickerListPage();
-        //Стикер "Акция" (красный цвет)
-        stickerSettings.sticker_Promotion.click();
-        stickerSettings.selectSettingPositionsInProductLists("T");
-        stickerSettings.selectSettingPositionsOnProductPage("T");
-        stickerSettings.statusActive.click();
-        addConditionOfPrice(stickerSettings);
-        stickerSettings.buttonSaveSticker.click();
-        //Стикер "Sale > 10% < 30%" (оранжевый цвет)
-        stickerSettings.dropDownToggle.click();
-        stickerSettings.toggleStickerList.click();
-        stickerSettings.sticker_SaleOrange.click();
-        stickerSettings.selectSettingPositionsInProductLists("T");
-        stickerSettings.selectSettingPositionsOnProductPage("T");
-        stickerSettings.statusActive.click();
-        stickerSettings.tab_DisplayOn.hover().click();
-        stickerSettings.selectDisplayOn_ShortList("small_size");
-        stickerSettings.buttonSaveSticker.click();
-        //Стикер "Популярный" (фиолетовый цвет)
-        stickerSettings.dropDownToggle.click();
-        stickerSettings.toggleStickerList.click();
-        stickerSettings.sticker_PopularProduct.click();
-        stickerSettings.selectSettingPositionsInProductLists("T");
-        stickerSettings.selectSettingPositionsOnProductPage("T");
-        stickerSettings.statusActive.click();
-        addConditionOfPrice(stickerSettings);
-        stickerSettings.buttonSaveSticker.click();
-        stickerSettings.gearWheel.click();
-        stickerSettings.generateStickerLinks.click();
-        Selenide.sleep(2000);
-        //Три нижних стикера
-        //Стикер "Высокий рейтинг" (оранжевый цвет)
-        stickerSettings.dropDownToggle.shouldBe(Condition.interactable).click();
-        stickerSettings.toggleStickerList.click();
-        stickerSettings.sticker_TopRated.click();
-        stickerSettings.selectSettingPositionsInProductLists("B");
-        stickerSettings.selectSettingPositionsOnProductPage("B");
-        stickerSettings.statusActive.click();
-        addConditionOfPrice(stickerSettings);
-        stickerSettings.buttonSaveSticker.click();
-        //Стикер "Бесплатная доставка" (цвет сине-белый)
-        stickerSettings.dropDownToggle.click();
-        stickerSettings.toggleStickerList.click();
-        stickerSettings.sticker_Free_Delivery.click();
-        stickerSettings.selectSettingPositionsInProductLists("B");
-        stickerSettings.selectSettingPositionsOnProductPage("B");
-        stickerSettings.statusActive.click();
-        addConditionOfPrice(stickerSettings);
-        stickerSettings.buttonSaveSticker.click();
-        stickerSettings.gearWheel.click();
-        stickerSettings.generateStickerLinks.click();
-        Selenide.sleep(2000);
-        //Стикер "Вес" (цвет серый)
-        stickerSettings.dropDownToggle.shouldBe(Condition.interactable).click();
-        stickerSettings.toggleStickerList.click();
-        stickerSettings.sticker_Weight.click();
-        stickerSettings.selectSettingPositionsInProductLists("B");
-        stickerSettings.selectSettingPositionsOnProductPage("B");
-        stickerSettings.statusActive.click();
-        stickerSettings.buttonSaveSticker.click();
 
-        //Настраиваем страницу товара
-        csCartSettings.navigateToEditingCategoryPage();
-        $x("//a[text()='AB: Телефоны']").click();
-        csCartSettings.statusActive_Category.click();
-        csCartSettings.button_Save.click();
-        csCartSettings.gearWheelOnTop.click();
-        csCartSettings.button_ViewProducts.click();
-        ProductSettings productSettings = new ProductSettings();
+        //Переходим на страницу редактирования товара
+        ProductSettings productSettings = csCartSettings.navigateToEditingProductPage();
+        productSettings.clickAndType_ProductSearch("Apple iPhone 14");
         $x("//td[@class='product-name-column wrap-word']//a[contains(text(), 'Apple iPhone 14')]").click();
-        productSettings.statusActive_Product.click();
-        productSettings.field_ListPrice.click();
-        productSettings.field_ListPrice.clear();
-        productSettings.field_ListPrice.sendKeys("2000");
         productSettings.selectProductTemplate("default_template");
-        productSettings.tab_Shippings.hover().click();
-        productSettings.clickAndType_ProductWeight("9");
         csCartSettings.navigateToStProductPage(1);
         csCartSettings.cookieNotice();
     }
 
     @Test(priority=2)
     public void TestCaseOne_ProductPage() {
+
         StProductPage stProductPage = new StProductPage();
         //Проверяем, что галерея мини-иконок вертикальная
         Assert.assertTrue($(".ab-vg-vertical-thumbnails").exists(), "Gallery of mini-icons is not Vertical!");
